43fe463e91b272d28032c08044be6b072fa27692
emersolbusinesssolutions <emersolbusinesssolutions@gmail.com>
2022-01-09 10:41:28 +0100

    languages forensic

--- components/Mobilenavbar.vue
+++ components/Mobilenavbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mobilenav">
-        <div>
+        <div v-b-toggle.sidebar-backdrop>
           <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M28.7984 13.3333H4.79837" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M28.7984 8H4.79837" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -11,5 +11,41 @@
         <div class="mobilelogo">
           <img src="~/assets/img/logo.png" alt="Brand Logo">
         </div>
+
+        <b-sidebar
+            id="sidebar-backdrop"
+            backdrop-variant="dark"
+            width="220px"
+            no-header
+            backdrop
+            shadow
+        >
+            <div class="px-3 pt-5">
+                <div class="sbnav">                    
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M24 8L8 24" stroke="#050F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M8 8L24 24" stroke="#050F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                    <div class="sibnav">
+                        <div class="sidebar">
+                            <NuxtLink to="/">Home</NuxtLink>
+                            <NuxtLink to="/howtoplay">How To Play</NuxtLink>
+                            <NuxtLink to="">Game Session</NuxtLink>
+                            <a v-b-toggle.my-collapse>Login Wallet</a>
+                            <b-collapse id="my-collapse">
+                                <ul>
+                                    <li>snak</li>
+                                    <li>dodo</li>
+                                </ul>
+                            </b-collapse>
+                            <a href="#contact">Contact</a>
+                            <a href="#about">About</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </b-sidebar>
+
     </div>
 </template>
